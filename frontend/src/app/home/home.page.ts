883f88f02b3ab5f00eee7bb045ebf14e5b899209
Donovan Solms <4567303+donovansolms@users.noreply.github.com>
2023-12-12 10:13:44 +0200

    Add initial frontend

---
+++ frontend/src/app/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class HomePage {
+  constructor() {}
+}
