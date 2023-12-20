ad233d87998f837b46943b8a80ca6882f531ff04
Donovan Solms <4567303+donovansolms@users.noreply.github.com>
2023-12-20 09:20:15 +0200

    Add initial mint page

---
+++ frontend/src/app/mint/mint.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-mint',
+  templateUrl: 'mint.page.html',
+  styleUrls: ['mint.page.scss'],
+  standalone: true,
+  imports: [IonContent, RouterModule],
+})
+export class MintPage {
+  constructor(private router: Router) {
+  }
+}
