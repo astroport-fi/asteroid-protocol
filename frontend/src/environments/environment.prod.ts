3fc2071bedc5f2af0d6b561c6ee9cd54de37d80e
Donovan Solms <4567303+donovansolms@users.noreply.github.com>
2023-12-26 09:08:09 +0200

    Update build to fix styling with --prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,59 @@
 export const environment = {
-  production: true
+  production: true,
+  domain: 'localhost:8100',
+  fees: {
+    protocol: {
+      receiver: "cosmos1y6338yfh4syssaglcgh3ved9fxhfn0jk4v8qtv",
+      amount: [
+        {
+          denom: "uatom",
+          amount: "10000"
+        }
+      ]
+    },
+    chain: {
+      gasLimit: "100000000",
+      amount: {
+        denom: "uatom",
+        amount: "10000",
+      },
+    }
+  },
+  api: {
+    endpoint: 'http://localhost:8080',
+    explorer: 'http://127.0.0.1:8665/chain/gaia/lcd/cosmos/tx/v1beta1/txs/',
+  },
+  chain: {
+    chainId: "gaialocal-1",
+    chainName: "Private Cosmos Hub",
+    rpc: "http://localhost:8665/chain/gaia/rpc",
+    rest: "http://localhost:8665/chain/gaia/lcd",
+    bip44: {
+      coinType: 118
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: 'cosmos',
+      bech32PrefixAccPub: 'cosmospub',
+      bech32PrefixValAddr: 'cosmosval',
+      bech32PrefixValPub: 'cosmosvalpub',
+      bech32PrefixConsAddr: 'cosmosvalcons',
+      bech32PrefixConsPub: 'cosmosvalconsconspub'
+    },
+    currencies: [{
+      coinDenom: 'atom',
+      coinMinimalDenom: 'uatom',
+      coinDecimals: 6
+    }],
+    feeCurrencies: [{
+      coinDenom: 'stake',
+      coinMinimalDenom: 'stake',
+      coinDecimals: 6,
+      gasPriceStep: { low: 0.01, average: 0.025, high: 0.04 }
+    }],
+    stakeCurrency: {
+      coinDenom: 'atom',
+      coinMinimalDenom: 'uatom',
+      coinDecimals: 6
+    },
+  },
 };
