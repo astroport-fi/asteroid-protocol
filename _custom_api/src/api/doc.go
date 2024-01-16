4fef82123a06e7408631518607f915ddfccbf557
Donovan Solms <4567303+donovansolms@users.noreply.github.com>
2024-01-17 00:32:01 +0200

    Drop old API

--- _custom_api/src/api/doc.go
+++
@@ -1,3 +0,0 @@
-// Package indexer implements a reference implementation of the indexer
-// following the Cosmos Inscriptions spec in the root of this repository
-package api
--- _custom_api/src/api/models/doc.go
+++
@@ -1,2 +0,0 @@
-// Package models implement database models for the indexer
-package models
