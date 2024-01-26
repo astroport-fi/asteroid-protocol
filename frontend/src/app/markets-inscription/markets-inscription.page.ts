dbdd14472f024f638e450de404b1986d75dae053
Donovan Solms <4567303+donovansolms@users.noreply.github.com>
2024-01-26 13:48:18 +0200

    Initial pages for inscription market

--- frontend/src/app/markets-inscription/markets-inscription.page.ts
+++ frontend/src/app/markets-inscription/markets-inscription.page.ts
@@ -15,20 +15,23 @@ import { SortEvent } from 'primeng/api';
 import { SellModalPage } from '../sell-modal/sell-modal.page';
 import { WalletService } from '../core/service/wallet.service';
 import { MarketplaceNoticeModalPage } from '../marketplace-notice/marketplace-notice-modal.page';
+import { GenericPreviewPage } from '../generic-preview/generic-preview.page';
+import { DropdownModule } from 'primeng/dropdown';
+import { ViewInscriptionPage } from '../view-inscription/view-inscription.page';
 
 @Component({
   selector: 'app-markets-inscription',
   templateUrl: './markets-inscription.page.html',
   styleUrls: ['./markets-inscription.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, DateAgoPipe, HumanTypePipe, DecimalPipe, HumanSupplyPipe, TokenDecimalsPipe, RouterLink, TableModule]
+  imports: [IonicModule, CommonModule, FormsModule, DateAgoPipe, HumanTypePipe, DecimalPipe, HumanSupplyPipe, TokenDecimalsPipe, RouterLink, TableModule, GenericPreviewPage, DropdownModule]
 })
 export class MarketsInscriptionPage implements OnInit {
 
   isLoading = true;
   isTableLoading: boolean = false;
   userAddress: string = '';
-  tokens: any = null;
+  inscriptions: any = null;
   offset = 0;
   limit = 20;
   total: number = 20000;
@@ -36,216 +39,215 @@ export class MarketsInscriptionPage implements OnInit {
   baseToken: any;
   chain: any;
 
+  priceRanges: string[] = ["0-10 ATOM", "10+ ATOM"];
+  selectedPriceRange: string | undefined;
+
+  blockRanges: string[] = ["Sub 100", "Sub 1 000", "Sub 10 000", "Sub 100 000"];
+  selectedBlockRange: string | undefined;
+
+  orderOptions: string[] = ["Newest", "Oldest"];
+  selectedOrder: string | undefined;
+
+  typeOptions: string[] = ["Image", "Video"];
+  selectedType: string | undefined;
+
   constructor(private activatedRoute: ActivatedRoute, private priceService: PriceService, private modalCtrl: ModalController, private walletService: WalletService) {
-    this.lastFetchCount = this.limit;
+    // this.lastFetchCount = this.limit;
     this.chain = Chain(environment.api.endpoint);
   }
 
   async ngOnInit() {
 
-    // Check if tis use has seen the notice
-    // if (localStorage.getItem('marketplace-notice') != 'shown') {
-    this.modalCtrl.create({
-      component: MarketplaceNoticeModalPage,
-    }).then(modal => {
-      modal.present();
-    });
-    // }
-
-    this.activatedRoute.params.subscribe(async params => {
-      if (await this.walletService.isConnected()) {
-        this.userAddress = (await this.walletService.getAccount()).address;
-      }
-
-      this.isLoading = true;
+    const result = await this.fetchInscriptions()
+    this.inscriptions = result.inscription;
+    this.isLoading = false;
 
 
-      const statusResult = await this.chain('query')({
-        status: [
-          {
-            where: {
-              chain_id: {
-                _eq: environment.chain.chainId
-              }
-            }
-          },
-          {
-            base_token: true,
-            base_token_usd: true,
-          }
-        ]
-      });
-      this.baseToken = statusResult.status[0];
-
-      const tokensResult = await this.chain('query')({
-        token: [
-          {
-            order_by: [
-              { id: order_by.desc }
-            ],
-            limit: 1
-          }, {
-            id: true,
-          }
-        ]
-      });
-      this.total = tokensResult.token[0].id;
-
-
-      const wsChain = Subscription(environment.api.wss);
-      wsChain('subscription')({
-        status: [
-          {
-            where: {
-              chain_id: {
-                _eq: environment.chain.chainId
-              }
-            }
-          },
-          {
-            base_token: true,
-            base_token_usd: true,
-          }
-        ]
-      }).on(({ status }) => {
-        this.baseToken = status[0];
-      });
-
-      this.isLoading = false;
-    });
   }
 
-  async listSale(event: any, ticker: string) {
-    event.stopPropagation();
-    const modal = await this.modalCtrl.create({
-      keyboardClose: true,
-      backdropDismiss: true,
-      component: SellModalPage,
+  async fetchInscriptions() {
 
-      componentProps: {
-        ticker: ticker
-      }
-    });
-    modal.present();
-  }
-
-  customSort(event: SortEvent) {
-    if (event.field == 'listings') {
-      event.data?.sort((data1, data2) => {
-        let value1 = data1["marketplace_cft20_details"].length;
-        let value2 = data2["marketplace_cft20_details"].length;
-        let result = null;
-
-        if (value1 == null && value2 != null) result = -1;
-        else if (value1 != null && value2 == null) result = 1;
-        else if (value1 == null && value2 == null) result = 0;
-        else if (typeof value1 === 'string' && typeof value2 === 'string') result = value1.localeCompare(value2);
-        else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
-
-        return event.order as number * result;
-      });
-
-    }
-
-    event.data?.sort((data1, data2) => {
-      let value1 = data1[event.field as string];
-      let value2 = data2[event.field as string];
-      let result = null;
-
-      if (value1 == null && value2 != null) result = -1;
-      else if (value1 != null && value2 == null) result = 1;
-      else if (value1 == null && value2 == null) result = 0;
-      else if (typeof value1 === 'string' && typeof value2 === 'string') result = value1.localeCompare(value2);
-      else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
-
-      return event.order as number * result;
-    });
-  }
-
-  async load(event: TableLazyLoadEvent) {
-    this.isTableLoading = true;
-
-    // Determine the sort order
-    let sortOrder = event.sortOrder === 1 ? 'asc' : 'desc';
-
-    // Build the order_by clause based on the event.sortField and sortOrder
-    let orderByClause: any = {};
-    if (event.sortField) {
-      orderByClause[event.sortField as string] = sortOrder;
-    } else {
-      // Default sorting, if no sortField is provided
-      orderByClause = { id: 'asc' };
-    }
-
-    let whereClause: any = {};
-    if (event.globalFilter) {
-      const globalFilter = event.globalFilter as string;
-      whereClause = {
-        _or: [
-          { name: { _like: `%${globalFilter}%` } },
-          { name: { _like: `%${globalFilter.toUpperCase()}%` } },
-          { ticker: { _like: `%${globalFilter}%` } },
-          { ticker: { _like: `%${globalFilter.toUpperCase()}%` } },
-        ]
-      };
-    }
-
-
-    const tokensResult = await this.chain('query')({
-      token: [
+    return this.chain('query')({
+      inscription: [
         {
-          offset: event.first,
-          limit: event.rows,
+          offset: 0,
+          limit: 50,
           order_by: [
-            orderByClause
+            {
+              id: order_by.asc
+            }
           ],
-          where: whereClause
         }, {
           id: true,
           transaction: {
             hash: true
           },
-          marketplace_cft20_details: [
-            {
-              where: {
-                marketplace_listing: {
-                  is_cancelled: {
-                    _eq: false
-                  },
-                  is_filled: {
-                    _eq: false
-                  }
-                }
-              }
-            },
-            {
-              id: true,
-            }
-          ],
-          token_holders: [
-            {
-              where: {
-                address: {
-                  _eq: this.userAddress
-                }
-              }
-            },
-            {
-              amount: true
-            }
-          ],
+          // transaction_hash: true,
+          current_owner: true,
           content_path: true,
-          name: true,
-          ticker: true,
-          decimals: true,
-          last_price_base: true,
-          volume_24_base: true,
+          content_size_bytes: true,
+          date_created: true,
+          is_explicit: true,
+          __alias: {
+            name: {
+              metadata: [{
+                path: '$.metadata.name'
+              },
+                true
+              ]
+            },
+            description: {
+              metadata: [{
+                path: '$.metadata.description'
+              },
+                true
+              ]
+            },
+            mime: {
+              metadata: [{
+                path: '$.metadata.mime'
+              },
+                true
+              ]
+            }
+          }
         }
       ]
     });
-    this.tokens = tokensResult.token;
-    this.isTableLoading = false;
   }
+
+  // async listSale(event: any, ticker: string) {
+  //   event.stopPropagation();
+  //   const modal = await this.modalCtrl.create({
+  //     keyboardClose: true,
+  //     backdropDismiss: true,
+  //     component: SellModalPage,
+
+  //     componentProps: {
+  //       ticker: ticker
+  //     }
+  //   });
+  //   modal.present();
+  // }
+
+  // customSort(event: SortEvent) {
+  //   if (event.field == 'listings') {
+  //     event.data?.sort((data1, data2) => {
+  //       let value1 = data1["marketplace_cft20_details"].length;
+  //       let value2 = data2["marketplace_cft20_details"].length;
+  //       let result = null;
+
+  //       if (value1 == null && value2 != null) result = -1;
+  //       else if (value1 != null && value2 == null) result = 1;
+  //       else if (value1 == null && value2 == null) result = 0;
+  //       else if (typeof value1 === 'string' && typeof value2 === 'string') result = value1.localeCompare(value2);
+  //       else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
+
+  //       return event.order as number * result;
+  //     });
+
+  //   }
+
+  //   event.data?.sort((data1, data2) => {
+  //     let value1 = data1[event.field as string];
+  //     let value2 = data2[event.field as string];
+  //     let result = null;
+
+  //     if (value1 == null && value2 != null) result = -1;
+  //     else if (value1 != null && value2 == null) result = 1;
+  //     else if (value1 == null && value2 == null) result = 0;
+  //     else if (typeof value1 === 'string' && typeof value2 === 'string') result = value1.localeCompare(value2);
+  //     else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
+
+  //     return event.order as number * result;
+  //   });
+  // }
+
+  // async load(event: TableLazyLoadEvent) {
+  //   this.isTableLoading = true;
+
+  //   // Determine the sort order
+  //   let sortOrder = event.sortOrder === 1 ? 'asc' : 'desc';
+
+  //   // Build the order_by clause based on the event.sortField and sortOrder
+  //   let orderByClause: any = {};
+  //   if (event.sortField) {
+  //     orderByClause[event.sortField as string] = sortOrder;
+  //   } else {
+  //     // Default sorting, if no sortField is provided
+  //     orderByClause = { id: 'asc' };
+  //   }
+
+  //   let whereClause: any = {};
+  //   if (event.globalFilter) {
+  //     const globalFilter = event.globalFilter as string;
+  //     whereClause = {
+  //       _or: [
+  //         { name: { _like: `%${globalFilter}%` } },
+  //         { name: { _like: `%${globalFilter.toUpperCase()}%` } },
+  //         { ticker: { _like: `%${globalFilter}%` } },
+  //         { ticker: { _like: `%${globalFilter.toUpperCase()}%` } },
+  //       ]
+  //     };
+  //   }
+
+
+  //   const tokensResult = await this.chain('query')({
+  //     token: [
+  //       {
+  //         offset: event.first,
+  //         limit: event.rows,
+  //         order_by: [
+  //           orderByClause
+  //         ],
+  //         where: whereClause
+  //       }, {
+  //         id: true,
+  //         transaction: {
+  //           hash: true
+  //         },
+  //         marketplace_cft20_details: [
+  //           {
+  //             where: {
+  //               marketplace_listing: {
+  //                 is_cancelled: {
+  //                   _eq: false
+  //                 },
+  //                 is_filled: {
+  //                   _eq: false
+  //                 }
+  //               }
+  //             }
+  //           },
+  //           {
+  //             id: true,
+  //           }
+  //         ],
+  //         token_holders: [
+  //           {
+  //             where: {
+  //               address: {
+  //                 _eq: this.userAddress
+  //               }
+  //             }
+  //           },
+  //           {
+  //             amount: true
+  //           }
+  //         ],
+  //         content_path: true,
+  //         name: true,
+  //         ticker: true,
+  //         decimals: true,
+  //         last_price_base: true,
+  //         volume_24_base: true,
+  //       }
+  //     ]
+  //   });
+  //   this.tokens = tokensResult.token;
+  //   this.isTableLoading = false;
+  // }
+
 
 
 }
