160ab22912c97a8f9967e7185f9b7690bcb1aa4c
Tomas Hanacek <tomas@astroport.foundation>
2024-02-29 19:39:18 +0100

    update asteroid sdk

--- app/app/hooks/useOperations.ts
+++ app/app/hooks/useOperations.ts
@@ -1,13 +1,25 @@
 import {
   CFT20Operations,
+  CFT20Protocol,
   InscriptionOperations,
+  InscriptionProtocol,
   MarketplaceOperations,
+  MarketplaceProtocol,
 } from '@asteroid-protocol/sdk'
+import { ProtocolFee } from '@asteroid-protocol/sdk/metaprotocol'
 import { useMemo } from 'react'
 import { clientOnly$ } from 'vite-env-only'
 import { useRootContext } from '~/context/root'
 import useAddress from '~/hooks/useAddress'
 import useAsteroidClient from '~/hooks/useAsteroidClient'
+
+function getFee(fee: ProtocolFee, useIbc: boolean): ProtocolFee | undefined {
+  if (useIbc) {
+    return
+  }
+
+  return { ...fee, receiver: 'cosmos1y6338yfh4syssaglcgh3ved9fxhfn0jk4v8qtv' }
+}
 
 export function useInscriptionOperations<T extends boolean = true>(
   multi = true as T,
@@ -24,6 +36,7 @@ export function useInscriptionOperations<T extends boolean = true>(
       new InscriptionOperations(chainId, address, {
         multi,
         useIbc,
+        fee: getFee(InscriptionProtocol.DEFAULT_FEE, useIbc),
       }),
     )
   }, [chainId, address, useIbc, multi])
@@ -44,6 +57,7 @@ export function useCFT20Operations<T extends boolean = true>(
       new CFT20Operations(chainId, address, {
         multi,
         useIbc,
+        fee: getFee(CFT20Protocol.DEFAULT_FEE, useIbc),
       }),
     )
   }, [chainId, address, useIbc, multi])
@@ -65,6 +79,7 @@ export function useMarketplaceOperations<T extends boolean = true>(
       new MarketplaceOperations(chainId, address, asteroidClient, {
         multi,
         useIbc,
+        fee: getFee(MarketplaceProtocol.DEFAULT_FEE, useIbc),
       }),
     )
   }, [chainId, address, useIbc, asteroidClient, multi])
