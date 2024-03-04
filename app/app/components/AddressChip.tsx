11f950a69bd4e8ed5e8679032c1148977012ae83
Tomas Hanacek <tomas@astroport.foundation>
2024-03-04 13:04:51 +0100

    show "You" in address chip

--- app/app/components/AddressChip.tsx
+++ app/app/components/AddressChip.tsx
@@ -1,8 +1,11 @@
 import { UserCircleIcon } from '@heroicons/react/24/solid'
 import { Link } from '@remix-run/react'
+import useAddress from '~/hooks/useAddress'
 import Address from './Address'
 
 export default function AddressChip({ address }: { address: string }) {
+  const currentAddress = useAddress()
+
   return (
     <Link
       to={`/app/wallet/${address}`}
@@ -10,7 +13,7 @@ export default function AddressChip({ address }: { address: string }) {
     >
       <UserCircleIcon className="size-6" />
       <span className="ml-2">
-        <Address address={address} />
+        {currentAddress === address ? 'You' : <Address address={address} />}
       </span>
     </Link>
   )
