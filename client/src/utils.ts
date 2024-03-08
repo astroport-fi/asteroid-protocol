0ba41b62da5205aa19699c462ab356684254a8e9
Tomas Hanacek <tomas.hanacek1@gmail.com>
2024-03-08 10:52:16 +0100

    client - add create collection and inscribe-csv commands

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,3 +1,4 @@
+import { parseFile } from '@fast-csv/parse'
 import fs from 'fs/promises'
 
 export async function fileExists(file: string) {
@@ -7,4 +8,16 @@ export async function fileExists(file: string) {
   } catch {
     return false
   }
+}
+
+export async function readCSV(
+  csvPath: string,
+): Promise<Record<string, string>[]> {
+  return new Promise((resolve, reject) => {
+    const rows: Record<string, string>[] = []
+    parseFile(csvPath, { headers: true })
+      .on('error', (error) => reject(error))
+      .on('data', (row) => rows.push(row))
+      .on('end', () => resolve(rows))
+  })
 }
