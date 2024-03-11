023a19182eb47b89bcec72e05b12b88bd7881011
Tomas Hanacek <tomas@astroport.foundation>
2024-03-11 11:43:59 +0100

    add pagination to inscriptions and deployed pages in wallet

--- app/app/routes/app.wallet.($address).deployed.tsx
+++ app/app/routes/app.wallet.($address).deployed.tsx
@@ -2,30 +2,36 @@ import { LoaderFunctionArgs, json } from '@remix-run/cloudflare'
 import { Link, useLoaderData } from '@remix-run/react'
 import { AsteroidClient } from '~/api/client'
 import GhostEmptyState from '~/components/GhostEmptyState'
+import Pagination from '~/components/Pagination'
 import { Tokens } from '~/components/TokensGallery'
+import usePagination from '~/hooks/usePagination'
 import { getAddress } from '~/utils/cookies'
+import { parsePagination } from '~/utils/pagination'
 
 export async function loader({ context, request, params }: LoaderFunctionArgs) {
   const asteroidClient = new AsteroidClient(context.cloudflare.env.ASTEROID_API)
+  const { offset, limit } = parsePagination(new URL(request.url).searchParams)
   let address = params.address
   if (!address) {
     address = await getAddress(request)
   }
   if (!address) {
-    return json({ tokens: [] })
+    return json({ tokens: [], pages: 0 })
   }
-  // @todo pagination
-  const tokens = await asteroidClient.getTokens(0, 500, {
+  const res = await asteroidClient.getTokens(offset, limit, {
     currentOwner: address,
   })
 
   return json({
-    tokens: tokens,
+    tokens: res.tokens,
+    pages: Math.ceil(res.count / limit),
   })
 }
 
 export default function WalletDeployed() {
   const data = useLoaderData<typeof loader>()
+  const [pagination, setPagination] = usePagination()
+
   if (data.tokens.length < 1) {
     return (
       <GhostEmptyState>
@@ -40,5 +46,15 @@ export default function WalletDeployed() {
       </GhostEmptyState>
     )
   }
-  return <Tokens tokens={data.tokens} />
+  return (
+    <div>
+      <Tokens tokens={data.tokens} />
+      <Pagination
+        pageCount={data.pages}
+        pagination={pagination}
+        setPagination={setPagination}
+        className="mt-8"
+      />
+    </div>
+  )
 }
--- app/app/routes/app.wallet.($address).inscriptions.tsx
+++ app/app/routes/app.wallet.($address).inscriptions.tsx
@@ -4,20 +4,24 @@ import { Link, useLoaderData } from '@remix-run/react'
 import { AsteroidClient } from '~/api/client'
 import GhostEmptyState from '~/components/GhostEmptyState'
 import { Inscriptions } from '~/components/Inscriptions'
+import Pagination from '~/components/Pagination'
+import usePagination from '~/hooks/usePagination'
 import { getAddress } from '~/utils/cookies'
+import { parsePagination } from '~/utils/pagination'
 
 export async function loader({ context, request, params }: LoaderFunctionArgs) {
   const asteroidClient = new AsteroidClient(context.cloudflare.env.ASTEROID_API)
+  const { offset, limit } = parsePagination(new URL(request.url).searchParams)
   let address = params.address
   if (!address) {
     address = await getAddress(request)
   }
   if (!address) {
-    return json({ inscriptions: [] })
+    return json({ inscriptions: [], pages: 0 })
   }
   const res = await asteroidClient.getInscriptions(
-    0,
-    500,
+    offset,
+    limit,
     {
       currentOwner: address,
     },
@@ -33,11 +37,14 @@ export async function loader({ context, request, params }: LoaderFunctionArgs) {
 
   return json({
     inscriptions: res.inscriptions,
+    pages: Math.ceil(res.count / limit),
   })
 }
 
 export default function WalletInscriptions() {
   const data = useLoaderData<typeof loader>()
+  const [pagination, setPagination] = usePagination()
+
   if (data.inscriptions.length < 1) {
     return (
       <GhostEmptyState>
@@ -52,5 +59,15 @@ export default function WalletInscriptions() {
       </GhostEmptyState>
     )
   }
-  return <Inscriptions className="w-full" inscriptions={data.inscriptions} />
+  return (
+    <div>
+      <Inscriptions className="w-full" inscriptions={data.inscriptions} />
+      <Pagination
+        pageCount={data.pages}
+        pagination={pagination}
+        setPagination={setPagination}
+        className="mt-8"
+      />
+    </div>
+  )
 }
