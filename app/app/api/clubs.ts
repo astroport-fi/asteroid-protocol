dd28595bc1c2dd0d0ec41466c1e44f0c8b3f779f
Tomas Hanacek <tomas@astroport.foundation>
2024-03-25 09:58:14 +0100

    add club images

--- app/app/api/clubs.ts
+++ app/app/api/clubs.ts
@@ -1,8 +1,15 @@
+import sub1kImage from '~/images/clubs/sub1k.png'
+import sub10kImage from '~/images/clubs/sub10k.png'
+import sub50kImage from '~/images/clubs/sub50k.png'
+import sub100Image from '~/images/clubs/sub100.png'
+import sub100kImage from '~/images/clubs/sub100k.png'
+
 export interface Club {
   id: string
   slug: string
   title: string
   range: number
+  image: string
 }
 
 const clubs: Club[] = [
@@ -11,36 +18,35 @@ const clubs: Club[] = [
     slug: 'sub-100',
     title: 'Sub 100',
     range: 100,
+    image: sub100Image,
   },
   {
     id: '1k',
     slug: 'sub-1k',
     title: 'Sub 1k',
     range: 1000,
-  },
-  {
-    id: '5k',
-    slug: 'sub-5k',
-    title: 'Sub 5k',
-    range: 5000,
+    image: sub1kImage,
   },
   {
     id: '10k',
     slug: 'sub-10k',
     title: 'Sub 10k',
     range: 10000,
+    image: sub10kImage,
   },
   {
     id: '50k',
     slug: 'sub-50k',
     title: 'Sub 50k',
     range: 50000,
+    image: sub50kImage,
   },
   {
     id: '100k',
     slug: 'sub-100k',
     title: 'Sub 100k',
     range: 100000,
+    image: sub100kImage,
   },
 ]
 
