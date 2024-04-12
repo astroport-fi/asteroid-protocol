29188ed06e1e5f36cf3963288ca0966ece933a38
Tomas Hanacek <tomas@astroport.foundation>
2024-04-12 16:07:08 +0200

    update migrations

--- indexer/migrations/20240411163432.sql
+++ indexer/migrations/20240411163432.sql
@@ -1,6 +1,6 @@
 -- Drop index "inscription_history_un" from table: "inscription_history"
-DROP INDEX "public"."inscription_history_un";
+ALTER TABLE inscription_history DROP CONSTRAINT inscription_history_un;
 -- Drop index "ith_tx_id" from table: "inscription_trade_history"
-DROP INDEX "public"."ith_tx_id";
+ALTER TABLE inscription_trade_history DROP CONSTRAINT ith_tx_id;
 -- Drop index "tth_tx_id" from table: "token_trade_history"
-DROP INDEX "public"."tth_tx_id";
+ALTER TABLE token_trade_history DROP CONSTRAINT tth_tx_id;
