53ee967943e30ad5d9fcd375d394f2b9bf056414
Tomas Hanacek <tomas@astroport.foundation>
2024-04-26 10:50:30 +0200

    indexer - add launchpad models

---
+++ indexer/src/indexer/models/launchpad_stage.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type LaunchpadStage struct {
+	ID           uint64         `gorm:"primary_key"`
+	Name         sql.NullString `gorm:"column:name"`
+	Description  sql.NullString `gorm:"column:description"`
+	CollectionID uint64         `gorm:"column:collection_id"`
+	LaunchpadID  uint64         `gorm:"column:launchpad_id"`
+	StartDate    time.Time      `gorm:"column:start_date"`
+	FinishDate   sql.NullTime   `gorm:"column:finish_date"`
+	Price        uint64         `gorm:"column:price"`
+	PerUserLimit uint64         `gorm:"column:per_user_limit"`
+}
+
+func (LaunchpadStage) TableName() string {
+	return "launchpad_stage"
+}
