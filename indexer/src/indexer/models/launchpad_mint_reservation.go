2b5f166bd3ac7a9cff8e147e39913d128dd0c43d
Tomas Hanacek <tomas@astroport.foundation>
2024-04-26 10:50:30 +0200

    indexer - add launchpad models

---
+++ indexer/src/indexer/models/launchpad_mint_reservation.go
@@ -0,0 +1,14 @@
+package models
+
+type LaunchpadMintReservation struct {
+	ID           uint64 `gorm:"primary_key"`
+	CollectionID uint64 `gorm:"column:collection_id"`
+	LaunchpadID  uint64 `gorm:"column:launchpad_id"`
+	StageID      uint64 `gorm:"column:stage_id"`
+	Address      string `gorm:"column:address"`
+	TokenId      uint64 `gorm:"column:token_id"`
+}
+
+func (LaunchpadMintReservation) TableName() string {
+	return "launchpad_mint_reservation"
+}
