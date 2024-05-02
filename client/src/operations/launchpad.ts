cb123f30fec4a72fbf729e471f0440dcb048ee10
Tomas Hanacek <tomas.hanacek1@gmail.com>
2024-05-02 17:26:59 +0200

    add launchpad metaprotocol and operations

---
+++ client/src/operations/launchpad.ts
@@ -0,0 +1,32 @@
+import LaunchpadProtocol, { MintStage } from '../metaprotocol/launchpad.js'
+import { OperationsBase, Options, getDefaultOptions } from './index.js'
+
+export class LaunchpadOperations<
+  T extends boolean = false,
+> extends OperationsBase<T> {
+  protocol: LaunchpadProtocol
+  address: string
+  options: Options<T>
+
+  constructor(
+    chainId: string,
+    address: string,
+    options: Options<T> = getDefaultOptions(),
+  ) {
+    super()
+    this.protocol = new LaunchpadProtocol(chainId, options.fee)
+    this.address = address
+    this.options = options
+  }
+
+  launch(collectionHash: string, supply: number, stages: MintStage[]) {
+    const operation = this.protocol.launch(collectionHash)
+    return this.prepareOperation(operation, { metadata: { supply, stages } })
+  }
+
+  // @todo signature
+  reserve(launchpadHash: string, mintStage: number) {
+    const operation = this.protocol.reserve(launchpadHash, mintStage)
+    return this.prepareOperation(operation)
+  }
+}
