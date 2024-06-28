f79d8d7d4b37241e995a2d951b4e7d2e7effd7a5
Tomas Hanacek <tomas@astroport.foundation>
2024-06-28 11:15:33 +0200

    update collections stats query

--- indexer/src/worker/workers/collections_stats.go
+++ indexer/src/worker/workers/collections_stats.go
@@ -34,21 +34,17 @@ func (w *CollectionsStatsWorker) Work(ctx context.Context, job *river.Job[Collec
 	query := `
 	INSERT INTO collection_stats(id, listed, floor_price, floor_price_1d_change, floor_price_1w_change, owners, supply, volume, volume_24h, volume_7d)
 		SELECT 
-			i.collection_id,
-			COUNT(mid.id) as listed, 
-			COALESCE(MIN(ml.total), 0) as floor_price,
-			(SELECT change FROM collection_floor_daily WHERE collection_id = i.collection_id LIMIT 1) as floor_price_1d_change,
-			(SELECT change FROM collection_floor_weekly WHERE collection_id = i.collection_id LIMIT 1) as floor_price_1w_change,
-			(SELECT COUNT(DISTINCT current_owner) as owners FROM inscription WHERE collection_id = i.collection_id),
-			(SELECT COUNT(id) as supply FROM inscription WHERE collection_id = i.collection_id),
-			(SELECT COALESCE(SUM(amount_quote), 0) as volume FROM inscription_trade_history ith INNER JOIN inscription r ON r.id = ith.inscription_id WHERE collection_id = i.collection_id),
-			(SELECT COALESCE(SUM(amount_quote), 0) as volume_24h FROM inscription_trade_history ith INNER JOIN inscription r ON r.id = ith.inscription_id WHERE collection_id = i.collection_id and NOW() - ith.date_created <= interval '24 HOURS'),
-			(SELECT COALESCE(SUM(amount_quote), 0) as volume_7d FROM inscription_trade_history ith INNER JOIN inscription r ON r.id = ith.inscription_id WHERE collection_id = i.collection_id and NOW() - ith.date_created <= interval '7 DAYS')
-		FROM inscription i
-		LEFT JOIN marketplace_inscription_detail mid ON i.id = mid.inscription_id
-		LEFT JOIN marketplace_listing ml ON ml.id = mid.listing_id
-		WHERE i.collection_id IS NOT NULL and (ml.id IS NULL OR (ml.is_cancelled IS FALSE AND ml.is_filled IS FALSE))
-		GROUP BY i.collection_id
+			cl.id,
+			(SELECT COUNT(mid.id) as listed FROM marketplace_inscription_detail mid INNER JOIN marketplace_listing ml ON ml.id = mid.listing_id INNER JOIN inscription i ON i.id = mid.inscription_id WHERE i.collection_id = cl.id AND ml.is_cancelled IS FALSE AND ml.is_filled IS FALSE),
+			(SELECT COALESCE(MIN(ml.total), 0) as floor_price FROM marketplace_inscription_detail mid INNER JOIN marketplace_listing ml ON ml.id = mid.listing_id INNER JOIN inscription i ON i.id = mid.inscription_id WHERE i.collection_id = cl.id AND ml.is_cancelled IS FALSE AND ml.is_filled IS FALSE),
+			(SELECT change FROM collection_floor_daily WHERE collection_id = cl.id and NOW() - "date" <= interval '24 hours' LIMIT 1) as floor_price_1d_change,
+			(SELECT change FROM collection_floor_weekly WHERE collection_id = cl.id and NOW() - "date" <= interval '7 days' LIMIT 1) as floor_price_1w_change,
+			(SELECT COUNT(DISTINCT current_owner) as owners FROM inscription WHERE collection_id = cl.id),
+			(SELECT COUNT(id) as supply FROM inscription WHERE collection_id = cl.id),
+			(SELECT COALESCE(SUM(amount_quote), 0) as volume FROM inscription_trade_history ith INNER JOIN inscription r ON r.id = ith.inscription_id WHERE collection_id = cl.id),
+			(SELECT COALESCE(SUM(amount_quote), 0) as volume_24h FROM inscription_trade_history ith INNER JOIN inscription r ON r.id = ith.inscription_id WHERE collection_id = cl.id and NOW() - ith.date_created <= interval '24 HOURS'),
+			(SELECT COALESCE(SUM(amount_quote), 0) as volume_7d FROM inscription_trade_history ith INNER JOIN inscription r ON r.id = ith.inscription_id WHERE collection_id = cl.id and NOW() - ith.date_created <= interval '7 DAYS')
+		FROM collection cl
 	ON CONFLICT (id) DO UPDATE SET listed = EXCLUDED.listed, supply = EXCLUDED.supply, owners = EXCLUDED.owners, volume = EXCLUDED.volume, volume_24h = EXCLUDED.volume_24h, volume_7d = EXCLUDED.volume_7d, floor_price = EXCLUDED.floor_price, floor_price_1w_change = EXCLUDED.floor_price_1w_change, floor_price_1d_change = EXCLUDED.floor_price_1d_change
 	`
 
