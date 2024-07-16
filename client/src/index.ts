0f8b1285a0e3467d78665c1fa1461a9baac17a1a
Tomas Hanacek <tomas.hanacek1@gmail.com>
2024-07-16 11:38:22 +0200

    export LaunchpadOperations and LaunchpadProtocol

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,6 +2,7 @@ export { CFT20Operations } from './operations/cft20.js'
 export { InscriptionOperations } from './operations/inscription.js'
 export { MarketplaceOperations } from './operations/marketplace.js'
 export { BridgeOperations } from './operations/bridge.js'
+export { LaunchpadOperations } from './operations/launchpad.js'
 
 export { default as CFT20Protocol } from './metaprotocol/cft20.js'
 export { default as InscriptionProtocol } from './metaprotocol/inscription.js'
@@ -11,6 +12,7 @@ export type {
 } from './metaprotocol/inscription.js'
 export { default as MarketplaceProtocol } from './metaprotocol/marketplace.js'
 export { default as BridgeProtocol } from './metaprotocol/bridge.js'
+export { default as LaunchpadProtocol } from './metaprotocol/launchpad.js'
 export type { TxData, TxFee, TxInscription } from './metaprotocol/tx.js'
 export type { ProtocolFee } from './metaprotocol/index.js'
 
