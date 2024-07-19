f3deeaed12fb204bc683cf0a13c4f7a8475a8538
Tomas Hanacek <tomas@astroport.foundation>
2024-07-19 14:17:06 +0200

    add start_date, finish_date and minted_supply to launchpad model

--- indexer/src/indexer/models/launchpad.go
+++ indexer/src/indexer/models/launchpad.go
@@ -1,18 +1,22 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 )
 
 type Launchpad struct {
-	ID            uint64    `gorm:"primary_key"`
-	ChainID       string    `gorm:"column:chain_id"`
-	Height        uint64    `gorm:"column:height"`
-	Version       string    `gorm:"column:version"`
-	TransactionID uint64    `gorm:"column:transaction_id"`
-	CollectionID  uint64    `gorm:"column:collection_id"`
-	MaxSupply     uint64    `gorm:"column:max_supply"`
-	DateCreated   time.Time `gorm:"column:date_created"`
+	ID            uint64       `gorm:"primary_key"`
+	ChainID       string       `gorm:"column:chain_id"`
+	Height        uint64       `gorm:"column:height"`
+	Version       string       `gorm:"column:version"`
+	TransactionID uint64       `gorm:"column:transaction_id"`
+	CollectionID  uint64       `gorm:"column:collection_id"`
+	MaxSupply     uint64       `gorm:"column:max_supply"`
+	MintedSupply  uint64       `gorm:"column:minted_supply"`
+	StartDate     sql.NullTime `gorm:"column:start_date"`
+	FinishDate    sql.NullTime `gorm:"column:finish_date"`
+	DateCreated   time.Time    `gorm:"column:date_created"`
 }
 
 func (Launchpad) TableName() string {
