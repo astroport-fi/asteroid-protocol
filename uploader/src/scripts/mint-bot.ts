5e78f41e9c4ca5b2310e4e6b3b5e1eff1f47a63d
Tomas Hanacek <tomas@astroport.foundation>
2024-07-24 17:52:06 +0200

    mint bot - add fee and stage price send messages, check if spend grant is sufficient

--- uploader/src/scripts/mint-bot.ts
+++ uploader/src/scripts/mint-bot.ts
@@ -5,13 +5,17 @@ import {
   broadcastTx,
 } from '@asteroid-protocol/sdk'
 import { getExecSendGrantMsg } from '@asteroid-protocol/sdk/msg'
-import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing'
+import { Coin, DirectSecp256k1HdWallet } from '@cosmjs/proto-signing'
 import { GasPrice } from '@cosmjs/stargate'
+import { SendAuthorization } from 'cosmjs-types/cosmos/bank/v1beta1/authz.js'
 import { AsteroidClient } from '../asteroid-client.js'
 import { loadConfig } from '../config.js'
+import { estimate } from '../cosmos-client.js'
 
 async function main() {
   const config = loadConfig()
+  const gasMultiplier = parseFloat(config.GAS_MULTIPLIER)
+  const feeMultiplier = 1
 
   // api
   const api = new AsteroidClient(config.ASTEROID_API)
@@ -34,15 +38,17 @@ async function main() {
   // get reservation
   const reservations = await api.getLaunchpadMintReservations()
   const reservation = reservations[0]
+  const launchpadTx = reservation.launchpad.transaction
+  const { collection } = reservation.launchpad
 
   // download inscription content and metadata
-  const metadataUrl = `https://${config.S3_BUCKET}.${config.S3_ENDPOINT}/${reservation.launchpad.transaction.hash}/${reservation.token_id}_metadata.json`
+  const metadataUrl = `https://${config.S3_BUCKET}.${config.S3_ENDPOINT}/${launchpadTx.hash}/${reservation.token_id}_metadata.json`
   const metadata = (await fetch(metadataUrl).then((res) =>
     res.json(),
   )) as NFTMetadata
   metadata.token_id = reservation.token_id
 
-  const inscriptionUrl = `https://${config.S3_BUCKET}.${config.S3_ENDPOINT}/${reservation.launchpad.transaction.hash}/${metadata.filename}`
+  const inscriptionUrl = `https://${config.S3_BUCKET}.${config.S3_ENDPOINT}/${launchpadTx.hash}/${metadata.filename}`
   const inscriptionBuffer = await fetch(inscriptionUrl).then((res) =>
     res.arrayBuffer(),
   )
@@ -55,7 +61,7 @@ async function main() {
     reservation.address,
   )
   const txData = operations.inscribeCollectionInscription(
-    reservation.launchpad.collection.transaction.hash,
+    collection.transaction.hash,
     new Uint8Array(inscriptionBuffer),
     metadata,
   )
@@ -63,16 +69,90 @@ async function main() {
   const msgs = txData.messages.map((msg) =>
     getExecSendGrantMsg(account.address, msg.value),
   )
+
+  if (reservation.stage.price) {
+    msgs.push(
+      getExecSendGrantMsg(account.address, {
+        fromAddress: reservation.address,
+        toAddress: collection.creator,
+        amount: [{ amount: `${reservation.stage.price}`, denom: 'uatom' }],
+      }),
+    )
+  }
+
   txData.messages = msgs
 
-  // sign and broadcast
-  const explorer = 'http://localhost:1316/cosmos/tx/v1beta1/txs/'
-  const res = await broadcastTx(
+  // estimate transactions fee
+  console.log('!!feeEstimation1')
+  const feeEstimation1 = await estimate(
     client,
     account.address,
     txData,
-    parseFloat(config.FEE_MULTIPLIER),
+    gasMultiplier,
+    feeMultiplier,
   )
+
+  txData.messages = msgs.concat([
+    getExecSendGrantMsg(account.address, {
+      fromAddress: reservation.address,
+      toAddress: account.address,
+      amount: feeEstimation1.amount as Coin[],
+    }),
+  ])
+
+  console.log('!!feeEstimation2')
+  const feeEstimation2 = await estimate(
+    client,
+    account.address,
+    txData,
+    gasMultiplier,
+    feeMultiplier,
+  )
+
+  txData.messages = msgs.concat([
+    getExecSendGrantMsg(account.address, {
+      fromAddress: reservation.address,
+      toAddress: account.address,
+      amount: feeEstimation2.amount as Coin[],
+    }),
+  ])
+
+  // check grant
+  const grants = await client
+    .forceGetQueryClient()
+    .authz.grants(
+      reservation.address,
+      account.address,
+      '/cosmos.bank.v1beta1.MsgSend',
+    )
+  const authorizationProto = grants.grants?.[0]?.authorization
+  if (!authorizationProto) {
+    // @todo
+    throw new Error('no authorization')
+  }
+  const sendAuthorization = SendAuthorization.decode(authorizationProto.value)
+  const authorizedCoin = sendAuthorization.spendLimit.find(
+    (coin) => coin.denom === 'uatom',
+  )
+  if (!authorizedCoin) {
+    // @todo
+    throw new Error('insufficient authorization amount')
+  }
+  const authorizedAmount = parseInt(authorizedCoin.amount, 10)
+  const requiredAmount =
+    parseInt(feeEstimation2.amount[0].amount, 10) +
+    (reservation.stage.price ?? 0) +
+    1
+
+  console.log(authorizedAmount, requiredAmount)
+  if (requiredAmount > authorizedAmount) {
+    // @todo
+    throw new Error('insufficient authorization amount')
+  }
+
+  // sign and broadcast
+  const explorer = 'http://localhost:1316/cosmos/tx/v1beta1/txs/'
+  const res = await broadcastTx(client, account.address, txData, gasMultiplier)
   console.log(`${explorer}${res.transactionHash}`)
 }
 
