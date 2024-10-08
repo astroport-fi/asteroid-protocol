fd14201ea2845bcb447c2bb98c392181b9adefac
Tomas Hanacek <tomas.hanacek1@gmail.com>
2024-10-08 19:15:10 +0200

    troll_post client

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,6 +3,7 @@ export { InscriptionOperations } from './operations/inscription.js'
 export { MarketplaceOperations } from './operations/marketplace.js'
 export { BridgeOperations } from './operations/bridge.js'
 export { LaunchpadOperations } from './operations/launchpad.js'
+export { TrollBoxOperations } from './operations/trollbox.js'
 
 export { default as CFT20Protocol } from './metaprotocol/cft20.js'
 export { default as InscriptionProtocol } from './metaprotocol/inscription.js'
@@ -13,6 +14,7 @@ export type {
 export { default as MarketplaceProtocol } from './metaprotocol/marketplace.js'
 export { default as BridgeProtocol } from './metaprotocol/bridge.js'
 export { default as LaunchpadProtocol } from './metaprotocol/launchpad.js'
+export { default as TrollBoxProtocol } from './metaprotocol/trollbox.js'
 export type { TxData, TxFee, TxInscription } from './metaprotocol/tx.js'
 export type { ProtocolFee } from './metaprotocol/index.js'
 
