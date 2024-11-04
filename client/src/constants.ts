d31f3781b9ba12d645964d62e01c913b18ba807a
T <tomas@astroport.foundation>
2024-11-05 05:37:20 +0800

    fix: fee receiver (#149)

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -4,5 +4,5 @@ export const DEFAULT_FEE_MULTIPLIER = 1.6
 export const DEFAULT_USE_EXTENSION_DATA = true
 export const DEFAULT_USE_IBC = true
 export const DEFAULT_FEE_RECEIVER =
-  'neutron1unc0549k2f0d7mjjyfm94fuz2x53wrx3px0pr55va27grdgmspcqgzfr8p'
+  'neutron18pu5afuah7gk7kc4mmv77000lgajcja994530d68skmmrc5ck8as0umur8'
 export const DEFAULT_FEE_AMOUNT = '1000'
