f84539d6b47a78692c5f4c9fc48dfd851bfe7cd7
T <tomas@astroport.foundation>
2024-11-05 21:10:46 +0800

    feat: add apps page (#150)

---
+++ app/app/images/background/index.ts
@@ -0,0 +1,5 @@
+export * from "./404.png";
+export * from "./503.png";
+export * from "./inscriptions-landscape.png";
+export * from "./inscriptions-landscape.webp";
+export * from "./meteor.webp";
---
+++ app/app/images/clubs/index.ts
@@ -0,0 +1,6 @@
+export * from "./clouds.png";
+export * from "./sub100.png";
+export * from "./sub100k.png";
+export * from "./sub10k.png";
+export * from "./sub1k.png";
+export * from "./sub5k.png";
